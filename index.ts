6972deb8312357ae37144fa405a82e34f0909f6b
Octa <ruarteoctavio8@gmail.com>
2022-09-26 16:43:45 -0300

    env vars

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import * as expres from "express";
 const app = expres();
+const isDev = process.env.NODE_ENV === "development";
 const port = process.env.PORT || 3000;
-
 app.get("/env", (req, res) => {
   res.json({ enviroment: process.env.NODE_ENV });
+});
+
+app.get("/db-env", (req, res) => {
+  res.json({ db: process.env.DB_URI });
 });
 
 app.listen(port, (req, res) => {
